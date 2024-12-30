66a8e609581b90ec02d4be480c9d3bcf041d0267
H-Was1 <hammadwasi.b@gmail.com>
2024-12-30 21:11:14 +0500

    FIX: added api  url in env file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://server-production-424b.up.railway.app/";
+const BASE_URL = import.meta.env.VITE_API_URL as string;
 
 export const fetchItems = async () => {
   const response = await axios.get(`${BASE_URL}items`);
